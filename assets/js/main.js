f15156ce047523e591bd50ec58f88281de1a51b5
rokcso <rokcsox@gmail.com>
2025-10-23 13:34:12 +0800

    Update contact email domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const user = "hi";
-  const domain = "rokcso.com";
+  const domain = "coryso.com";
   const emailElement = document.getElementById("email-contact");
   if (emailElement) {
     emailElement.innerHTML = `<a href="mailto:${user}@${domain}">${user}@${domain}</a>`;
